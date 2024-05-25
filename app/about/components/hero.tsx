cad4dcf4845fc157ca288a37b87d3608f3643030
cosugi <cosugi89@gmail.com>
2024-05-26 08:32:49 +0900

    init

--- app/about/components/hero.tsx
+++ app/about/components/hero.tsx
@@ -11,7 +11,7 @@ export default function hero() {
         <p className='text-muted-foreground mb-6'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Et at recusandae quasi fuga, voluptate quo laboriosam odit? Quam, voluptate debitis sit, nisi quae, quibusdam architecto facilis soluta placeat voluptas est?
         </p>
-        <Image src={heroimage} alt="heroimage"/>
+        {/* <Image src={heroimage} alt="heroimage"/> */}
         <Button>お問い合わせ</Button>
       </div>
     </div>
