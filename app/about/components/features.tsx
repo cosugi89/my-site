1b71f5c30e24c9f4291e2190dadc3c660c71d19a
cosugi <cosugi89@gmail.com>
2024-05-25 21:32:29 +0900

    fix(about): リンクタグのアクセシビリティ対応

--- app/about/components/features.tsx
+++ app/about/components/features.tsx
@@ -10,7 +10,10 @@ export default function features() {
           <div className='border rounded-md p-6 shadow space-y-3 relative'>
             <div className='aspect-video bg-muted'></div>
             <h2>
-              記事タイトル<Link href="/" className="absolute inset-0"></Link>
+              <Link href="/">
+                記事タイトル
+                <span className="absolute inset-0"></span>
+              </Link>
             </h2>
             <p>Lorem ipsum dolor sit amet.</p>
             <button>タグA</button>
